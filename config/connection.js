ea11f141baffffd66493ba77d4f0dae15c7c697e
Cara R <cara.rosa.02@gmail.com>
2024-01-02 12:12:03 -0600

    fixed typo in mongodb_uri

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const { connect, connection } = require('mongoose');
 
-const connectionString = process.env.MONDGODB_URI || 'mongodb://127.0.0.1:27017/networkDB';
+const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/networkDB';
 
 connect(connectionString);
 
